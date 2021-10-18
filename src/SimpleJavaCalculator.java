ef88c91352ec6e63acc58f7057657e6f9ba622a0
dmytrosmlk <dimasmolak70@gmail.com>
2021-10-18 20:25:54 +0300

    Math func and UI merge (main)

--- src/SimpleJavaCalculator.java
+++ src/SimpleJavaCalculator.java
@@ -1,2 +1,9 @@
+package simplejavacalculator;
 public class SimpleJavaCalculator {
+    public static void main(String[] args) {
+
+            UI uiCal = new UI();
+            uiCal.init();
+
+    }
 }
